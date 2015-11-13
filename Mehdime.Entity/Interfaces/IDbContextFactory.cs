7721d1fc1bb33062d4948c962b4bff83ffb253b8
Ben Ramey <Ben.Ramey@vml.com>
2015-11-13 13:04:50 -0600

    Add class constraint to be consistent on IDbContextFactory

--- Mehdime.Entity/Interfaces/IDbContextFactory.cs
+++ Mehdime.Entity/Interfaces/IDbContextFactory.cs
@@ -33,6 +33,6 @@ namespace Mehdime.Entity
     /// </remarks>
     public interface IDbContextFactory
     {
-		TDbContext CreateDbContext<TDbContext>() where TDbContext : IDbContext;
+		TDbContext CreateDbContext<TDbContext>() where TDbContext : class, IDbContext;
     }
 }
